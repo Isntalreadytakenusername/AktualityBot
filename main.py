2e29e811a0796186e477c71d023f73897914f4db
Vladyslav Romanov <51272775+Isntalreadytakenusername@users.noreply.github.com>
2022-06-09 13:29:05 +0000

    tested sending messages to a channel

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import os
+import telebot
+import time
+import requests
 
-API_KEY = os.environ['API_KEY']
+while 1:
+  API_KEY = os.environ['API_KEY']
+  
+  bot = telebot.TeleBot(API_KEY)
+  
+  #bot.send_message(301011944696936, "Test message")
+  requests.post('https://api.telegram.org/bot'+API_KEY+'/sendMessage?chat_id=-1001538579646&text=Hello World!')
+  print("Message sent")
+  time.sleep(5)
