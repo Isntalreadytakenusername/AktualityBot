3cceabd48600e8e71e7c44c2b08b9ffc6e0d1e9c
Vladyslav Romanov <51272775+Isntalreadytakenusername@users.noreply.github.com>
2022-06-09 14:44:57 +0000

    bot is set up to send new posts from fei.tuke.sk

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import os
-import telebot
+import pandas
 import time
-import requests
 
-while 1:
-  API_KEY = os.environ['API_KEY']
-  
-  bot = telebot.TeleBot(API_KEY)
-  
-  #bot.send_message(301011944696936, "Test message")
-  requests.post('https://api.telegram.org/bot'+API_KEY+'/sendMessage?chat_id=-1001538579646&text=Hello World!')
-  print("Message sent")
-  time.sleep(5)
+#we need to run the scraper from here and
+# all changing files will be here
+os.chdir('aktuality_scrapers')
+
+while True:
+    print("Checking for new posts...")
+    #check if the file exists delete
+    if os.path.exists('aktuality.csv'):
+        os.remove('aktuality.csv')
+        # read the file as pandas dataframe
+
+    os.system('scrapy crawl fei -o aktuality.csv')
+    aktuality_df = pandas.read_csv('aktuality.csv')
+    print("The last post is acquired")
+
+    if os.path.exists('aktuality_last.csv'):
+        aktuality_last_df = pandas.read_csv('aktuality_last.csv')
+        if aktuality_df["text"][0] == aktuality_last_df["text"][0]:
+            print("No new posts")
+        else:
+            print("New post found")
+            os.remove('aktuality_last.csv')
+            aktuality_df.to_csv('aktuality_last.csv', index=False)
+            os.system("python ../telegram_bot.py")
+    else:
+        aktuality_df.to_csv('aktuality_last.csv', index=False)
+        os.system("python ../telegram_bot.py")
+
+    for i in range(60):
+      print(f'Waiting {60-(i+1)} minutes before the next check')
+      time.sleep(60)
---
+++ telegram_bot.py
@@ -0,0 +1,32 @@
+import os
+import telebot
+import requests
+import pandas
+
+#read csv file to dataframe
+aktuality_last_df = pandas.read_csv('aktuality_last.csv')
+
+#prepare text to be posted
+
+title = aktuality_last_df["title"][0]
+date = aktuality_last_df["date"][0]
+date = f"<code>{date}</code>"
+image = aktuality_last_df["image"][0]
+text = aktuality_last_df["text"][0]
+link_to_aktuality_fei = "<a href = 'https://www.fei.tuke.sk/sk/studium/aktuality'><u><b>Aktuality FEI</b></u></a>"
+
+if len(image) == 0:
+  image = ""
+else:
+  image = f"<a href = '{image}'> <i> image </i> </a>"
+
+text_to_post = f"<b>{title}</b>\n{date}\n{text}\n\n\n {link_to_aktuality_fei}\n{image}"
+
+#prepare bot
+API_KEY = os.environ['API_KEY']
+bot = telebot.TeleBot(API_KEY)
+
+#send the message
+bot.send_message(chat_id = '@fei_tuke_aktuality', text = text_to_post, parse_mode = 'HTML')
+#requests.post(f'https://api.telegram.org/bot{API_KEY}/sendMessage?chat_id=-1001538579646&text={text_to_post}')
+print("Message sent")
