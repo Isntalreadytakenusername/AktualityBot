99c0446cccf0bf406f0d8ce82f6da3569bed84da
Vladyslav Romanov <51272775+Isntalreadytakenusername@users.noreply.github.com>
2022-06-11 13:43:39 +0200

    corrected link in text of message

--- aktuality_scrapers/aktuality/spiders/mais.py
+++ aktuality_scrapers/aktuality/spiders/mais.py
@@ -9,5 +9,5 @@ class MaisSpider(scrapy.Spider):
     def parse(self, response):
         text = response.xpath("(//p[contains(@class, 'newsPriority')])[1]/a/text()").extract_first()
         date = response.xpath("(//p[contains(@class, 'newsPriority')])[1]/span[@class = 'xSmall']/text()").extract_first()
-        text = text + '\n\n' + 'student.tuke.sk' + response.xpath("(//p[contains(@class, 'newsPriority')])[1]/a/text()").extract_first()
+        text = text + '\n\n' + 'student.tuke.sk' + response.xpath("(//p[contains(@class, 'newsPriority')])[1]/a/@href").extract_first()
         yield {'text': text, 'date': date}
