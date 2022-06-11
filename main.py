c77ddc1c73f730dc1cd35055e355f39fd904b526
Vladyslav Romanov <51272775+Isntalreadytakenusername@users.noreply.github.com>
2022-06-11 17:44:07 +0000

    name corrected, tuke_oznamy is working

--- main.py
+++ main.py
@@ -15,6 +15,6 @@ os.chdir('aktuality_scrapers')
 
 
 news_checkers_list = [NewsChecker("fei", "aktuality.csv", "API_KEY", "@fei_tuke_aktuality"), NewsChecker("mais", "mais.csv", "API_KEY", "@fei_tuke_aktuality", True, False, True, False, False),
-                       NewsChecker("tuke_oznamy", "tuke_oznamy", "API_KEY", "@fei_tuke_aktuality", True, True, True, False, True)]
+                       NewsChecker("tuke_oznamy", "tuke_oznamy.csv", "API_KEY", "@fei_tuke_aktuality", True, True, True, False, True)]
 process = CheckNewsProcess(news_checkers_list)
 process.run()
