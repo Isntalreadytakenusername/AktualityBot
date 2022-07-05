1298ec722dc846aaeb295940d826c0aa8291ada9
Vladyslav Romanov <51272775+Isntalreadytakenusername@users.noreply.github.com>
2022-07-05 16:58:12 +0200

    kpi web site is added

--- aktuality_scrapers/aktuality/spiders/kpi_udalosti.py
+++ aktuality_scrapers/aktuality/spiders/kpi_udalosti.py
@@ -4,7 +4,12 @@ import scrapy
 class KpiUdalostiSpider(scrapy.Spider):
     name = 'kpi_udalosti'
     allowed_domains = ['kpi.fei.tuke.sk']
-    start_urls = ['http://kpi.fei.tuke.sk/']
+    start_urls = ['https://kpi.fei.tuke.sk/sk/udalosti']
 
     def parse(self, response):
-        pass
+        title = response.xpath("(//span[@class = 'field-content'])[1]/a/text()").extract_first()
+        link = "https://kpi.fei.tuke.sk" + response.xpath("(//span[@class = 'field-content'])[1]/a/@href").extract_first()
+        text = response.xpath("(//div[@class = 'views-field views-field-body'])[1]/span/text()").extract_first()
+        date = response.xpath("(//span[@class = 'field-content news-date'])[1]/text()").extract_first()
+        image = response.xpath("(//img[contains(@src, 'header')])[1]/@src").extract_first()
+        yield {'title': title, 'link': link, 'text': text, 'date': date, 'image': image}
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ os.chdir('aktuality_scrapers')
 
 
 news_checkers_list = [NewsChecker("fei", "aktuality.csv", "API_KEY", "@fei_tuke_aktuality"), NewsChecker("mais", "mais.csv", "API_KEY", "@fei_tuke_aktuality", True, False, True, False, False),
-                       NewsChecker("tuke_oznamy", "tuke_oznamy.csv", "API_KEY", "@fei_tuke_aktuality", True, True, True, False, True), NewsChecker("tuke_aktuality", "tuke_aktuality.csv", "API_KEY", "@fei_tuke_aktuality", True, True, True, False, True)]
+                       NewsChecker("tuke_oznamy", "tuke_oznamy.csv", "API_KEY", "@fei_tuke_aktuality", True, True, True, False, True), NewsChecker("tuke_aktuality", "tuke_aktuality.csv", "API_KEY", "@fei_tuke_aktuality", True, True, True, False, True),
+                        NewsChecker('kpi_aktuality', 'kpi_aktuality.csv', 'API_KEY', '@fei_tuke_aktuality', True, True, True, True, True), NewsChecker('kpi_udalosti', 'kpi_udalosti.csv', 'API_KEY', '@fei_tuke_aktuality', True, True, True, True, True), NewsChecker('kpi_uspechy_katedry', 'kpi_uspechy_katedry.csv', 'API_KEY', '@fei_tuke_aktuality', True, True, True, True, True)]
 process = CheckNewsProcess(news_checkers_list)
 process.run()
