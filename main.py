5b50020ded4acc3e41079d9d32c944e44294c955
Vladyslav Romanov <51272775+Isntalreadytakenusername@users.noreply.github.com>
2022-07-17 09:36:10 +0000

    Removed images as links are rendered anyways

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from NewsCheckers import NewsChecker
 os.chdir('aktuality_scrapers')
 
 
-news_checkers_list = [NewsChecker("fei", "aktuality.csv", "API_KEY", "@fei_tuke_aktuality"), NewsChecker("mais", "mais.csv", "API_KEY", "@fei_tuke_aktuality", True, False, True, False, False),
+news_checkers_list = [NewsChecker("fei", "aktuality.csv", "API_KEY", "@fei_tuke_aktuality", True, True, True, False, True), NewsChecker("mais", "mais.csv", "API_KEY", "@fei_tuke_aktuality", True, False, True, False, False),
                        NewsChecker("tuke_oznamy", "tuke_oznamy.csv", "API_KEY", "@fei_tuke_aktuality", True, True, True, False, True), NewsChecker("tuke_aktuality", "tuke_aktuality.csv", "API_KEY", "@fei_tuke_aktuality", True, True, True, False, True),
-                        NewsChecker('kpi_aktuality', 'kpi_aktuality.csv', 'API_KEY', '@fei_tuke_aktuality', True, True, True, True, True), NewsChecker('kpi_udalosti', 'kpi_udalosti.csv', 'API_KEY', '@fei_tuke_aktuality', True, True, True, True, True), NewsChecker('kpi_uspechy_katedry', 'kpi_uspechy_katedry.csv', 'API_KEY', '@fei_tuke_aktuality', True, True, True, True, True)]
+                        NewsChecker('kpi_aktuality', 'kpi_aktuality.csv', 'API_KEY', '@fei_tuke_aktuality', True, True, True, False, True), NewsChecker('kpi_udalosti', 'kpi_udalosti.csv', 'API_KEY', '@fei_tuke_aktuality', True, True, True, False, True), NewsChecker('kpi_uspechy_katedry', 'kpi_uspechy_katedry.csv', 'API_KEY', '@fei_tuke_aktuality', True, True, True, False, True)]
 process = CheckNewsProcess(news_checkers_list)
 process.run()
