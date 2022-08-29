786d4a0cc6e57837a9a17f1fa35ead1f0dfda845
Vladyslav Romanov <51272775+Isntalreadytakenusername@users.noreply.github.com>
2022-08-29 13:27:56 +0200

    add history to remember what was sent

--- NewsCheckers.py
+++ NewsCheckers.py
@@ -22,6 +22,11 @@ class NewsChecker:
         self.telegram_bot = TelegramBot(self.last_scraper_output_file, environment_variable_with_api_key_for_bot, chat_id, is_text, is_title, is_date, is_image, is_link)
         self.telegram_bot.prepare_bot("API_KEY")
 
+        # if there is no file History.xlsx, create it with columns text and date using pandas
+        if not os.path.exists("History.xlsx"):
+            df = pd.DataFrame(columns=["text", "date"])
+            df.to_excel("History.xlsx", index=False)
+
 
 
     def check_for_new_posts(self):
@@ -53,11 +58,26 @@ class NewsChecker:
                 print("No new posts")
             else:
                 print("New post found")
+
+                # now check whether the post was already recorded to History.xlsx. This is needed becouse new post may be deleted and the last post that was already published will be published
+                # again. To avoid this, we check whether the post is already in History.xlsx
+                history_df = pd.read_excel("History.xlsx")
+                # check whether the post is already in History.xlsx using text and date fields. Check each row in History.xlsx and compare it to the last post (text and date)
+                for i in range(len(history_df)):
+                    if history_df["text"][i] == self.scraper_output_df["text"][0] and history_df["date"][i] == self.scraper_output_df["date"][0]:
+                        print("The post was already sent")
+                        return
+                
+                # if the post is not in History.xlsx, write it to History.xlsx and send it with the bot
+                history_df = history_df.append({"text": self.scraper_output_df["text"][0], "date": self.scraper_output_df["date"][0]}, ignore_index=True)
+
                 os.remove(self.last_scraper_output_file)
                 self.scraper_output_df.to_csv(self.last_scraper_output_file, index=False)
-                # check if post is less than 2 days old
+
+                # check if post is less than 2 days old to avoid sending old posts again if the last post is deleted
                 if self.scraper_output_df["date"][0] > (time.time() - (60*60*24*2)):
-                    self.telegram_bot.send_message()
+                    print("==============================THE MESSAGE IS SENT. UNCOMMENT LATER====================================")
+                    # self.telegram_bot.send_message()
                 # self.telegram_bot.send_bots_message()
         else:
             self.scraper_output_df.to_csv(self.last_scraper_output_file, index=False)
