647badfc59fb6dcf25e6fed210b424984ffc7d1d
Vladyslav Romanov <vlad.vik.romanov@gmail.com>
2024-08-24 16:59:53 +0200

    remove tuke.sk bots for now

--- main.py
+++ main.py
@@ -15,15 +15,14 @@ os.chdir('aktuality_scrapers')
 telegram_channel_id = '@fei_tuke_aktuality'
 telegram_channel_id = '@aktuality_testing'
 
-# # Temporarily removed tuke_oznamy as there is a mysterious problem with tuke oznamy and I have an exam period
-# news_checkers_list = [NewsChecker("fei", "aktuality.csv", "API_KEY", telegram_channel_id, True, True, True, False, True), NewsChecker("mais", "mais.csv", "API_KEY", telegram_channel_id, True, False, True, False, False),
-#                        NewsChecker("tuke_aktuality", "tuke_aktuality.csv", "API_KEY", telegram_channel_id, True, True, True, False, True),
-#                         NewsChecker('kpi_aktuality', 'kpi_aktuality.csv', 'API_KEY', telegram_channel_id, True, True, True, False, True), NewsChecker('kpi_udalosti', 'kpi_udalosti.csv', 'API_KEY', telegram_channel_id, True, True, True, False, True), NewsChecker('kpi_uspechy_katedry', 'kpi_uspechy_katedry.csv', 'API_KEY', telegram_channel_id, True, True, True, False, True)]
-
-# The old version with tuke_oznamy
+# Temporarily removed the ones using tuke.sk
 news_checkers_list = [NewsChecker("fei", "aktuality.csv", "API_KEY", telegram_channel_id, True, True, True, False, True), NewsChecker("mais", "mais.csv", "API_KEY", telegram_channel_id, True, False, True, False, False),
-                       NewsChecker("tuke_oznamy", "tuke_oznamy.csv", "API_KEY", "@aktuality_testing", True, True, True, False, True), NewsChecker("tuke_aktuality", "tuke_aktuality.csv", "API_KEY", telegram_channel_id, True, True, True, False, True),
                         NewsChecker('kpi_aktuality', 'kpi_aktuality.csv', 'API_KEY', telegram_channel_id, True, True, True, False, True), NewsChecker('kpi_udalosti', 'kpi_udalosti.csv', 'API_KEY', telegram_channel_id, True, True, True, False, True), NewsChecker('kpi_uspechy_katedry', 'kpi_uspechy_katedry.csv', 'API_KEY', telegram_channel_id, True, True, True, False, True)]
+
+# # The old version with all tuke.sk
+# news_checkers_list = [NewsChecker("fei", "aktuality.csv", "API_KEY", telegram_channel_id, True, True, True, False, True), NewsChecker("mais", "mais.csv", "API_KEY", telegram_channel_id, True, False, True, False, False),
+#                        NewsChecker("tuke_oznamy", "tuke_oznamy.csv", "API_KEY", "@aktuality_testing", True, True, True, False, True), NewsChecker("tuke_aktuality", "tuke_aktuality.csv", "API_KEY", telegram_channel_id, True, True, True, False, True),
+#                         NewsChecker('kpi_aktuality', 'kpi_aktuality.csv', 'API_KEY', telegram_channel_id, True, True, True, False, True), NewsChecker('kpi_udalosti', 'kpi_udalosti.csv', 'API_KEY', telegram_channel_id, True, True, True, False, True), NewsChecker('kpi_uspechy_katedry', 'kpi_uspechy_katedry.csv', 'API_KEY', telegram_channel_id, True, True, True, False, True)]
 process = CheckNewsProcess(news_checkers_list)
 process.run()
 
